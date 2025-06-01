3eb96cf669d13c46e97bcc86b67c4f286c709b39
Akweleyp <patriciaakweleyn@gmail.com>
2025-06-01 17:15:43 +0000

    imported dotenv

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
+
+import dotenv from "dotenv"
 import cors from "cors"
 import medicationRouter from "./router/medicationroutes.js";
 import mongoose from "mongoose";
 import symptomRouter from "./router/symptomsroutes.js";
 import userRouter from "./router/userroutes.js";
+
+
+// Load environment Variables 
+dotenv.config();
+
+
+
 
 // make database connection
 await mongoose.connect(process.env.MONGO_URI);
